b7ace0fab21b403722b9995f0dc55e7e7ead329a
Pieter <pieter@sensingit.nl>
2017-10-12 15:22:54 +0200

    init

---
+++ javaeetest/src/main/java/nl/han/pieter/HelloWorld.java
@@ -0,0 +1,26 @@
+package nl.han.pieter;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class HelloWorld {
+    private String username = "pieter";
+    private String password = "test123";
+
+
+    @Path("hello")
+    @GET
+    public String getMessage() {
+        return "Mandatory hello world";
+    }
+
+    @Path("login")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(String username, String password) {
+        return Response.status(200).build();
+    }
+}
